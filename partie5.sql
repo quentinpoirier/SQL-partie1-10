dafcc04597b4e431f005c32e01dcf31fb100c10a
unknown <quentin-poirier@live.fr>
2020-07-20 10:38:01 +0200

    dernière maj après correction

--- partie5.sql
+++ partie5.sql
@@ -12,7 +12,7 @@ exercice 4: SELECT * FROM `languages` WHERE `id` IN (3, 5, 7);
 exercice 5: SELECT * FROM `languages` WHERE `language` = 'javaScript' LIMIT 2;
 
 exercice 6: SELECT * FROM `languages` WHERE NOT `language` = 'PHP';
-	    SELECT * FROM `languages` WHERE `language` != 'PHP';
+	    	SELECT * FROM `languages` WHERE `language` != 'PHP';
 
 exercice 7: SELECT * FROM `languages` ORDER BY `language`;
- 	    SELECT * FROM `languages` ORDER BY DESC `language`;
+ 	    	SELECT * FROM `languages` ORDER BY DESC `language`;
